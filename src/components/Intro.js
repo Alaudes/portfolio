b101d7e0efbb44b2031b57eae27671bbf1c5d44d
Alaudes <farhat.a@live.com>
2021-08-12 13:11:21 +0200

    first version of intro

---
+++ src/components/Intro.js
@@ -0,0 +1,35 @@
+function Intro() {
+    return (
+        <section className="intro">
+            <div>
+                <h1>hi</h1>
+                <p> 
+                    Welcome to my (Farhat Abdad) portfolio!
+                    <br/>
+                    I am a 23 year old <i>front-end</i> developer
+                    <br/>
+                    based Amsterdam, The Netherlands.
+                </p>
+                
+            </div>
+            <div>
+                <div>
+                    <hr/>
+                    <p>
+                        At the moment interested in most things JS and stuff like user-experience and Animations.
+                    </p>
+                </div>
+                <div>
+                    <hr/>
+                    <p> 
+                        As for interests?
+                        <br/>
+                        I like to keep myself busy with playing games, reading books, building model gundam or lego sets and mechanical keyboards!
+                    </p>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Intro;
---
+++ src/components/Navigation.js
@@ -0,0 +1,21 @@
+function Navigation() {
+    return (
+        <nav>
+            <div className="nav-container">
+                <div className="nav-top">
+                    <img src="/logo.svg" className="logo" alt="logo" />
+                    <hr />
+                    <ul>
+                        <li>Projects</li>
+                        <li>Experience</li>
+                        <li>Education</li>
+                        <li>Skills</li>
+                        <li>Contact</li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default Navigation;
